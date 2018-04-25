bf15d45a6851dfa0e145f9d14724ef18188ecc60
Karoliina Salminen <karoliina.salminen@huawei.com>
2018-04-25 12:27:35 +0300

    Added Makefile and queue example extended to have push and pop.

--- tutorials/Tutorial2_queue/ActivationQueueExample.cpp
+++ tutorials/Tutorial2_queue/ActivationQueueExample.cpp
@@ -10,18 +10,44 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+#include <map>
+
 
 class Point3
 {
     public:
+    Point3():x(0),y(0),z(0){
 
-    std::tuple<int,int,int> p;
+    }
+    Point3(int xx, int yy, int zz):x(xx),y(yy),z(zz) {
+        
+    }
+    Point3(std::tuple<int,int,int> p){
+        x = std::get<0>(p); 
+        y = std::get<1>(p);
+        z = std::get<2>(p);
+    }
 
+    int x=0;
+    int y=0;
+    int z=0;
 };
 
 class ActivationQueue
 {
     public: 
+    void push(std::pair<Point3,float> address){
+        q.push(address);
+    }
+
+    std::pair<Point3,float> pop(){
+        std::pair<Point3,float> p;
+        if(!q.empty()){
+            p = q.front();
+            q.pop();
+        }
+        return p;
+    }
 
     std::queue<std::pair<Point3,float>> q;
 
@@ -33,5 +59,8 @@ ActivationQueue activationQueue;
 
 int main (){
     std::cout << "Activation queue demonstrator\n";
+    std::pair<Point3,float> activation = std::make_pair<Point3,float>(Point3(-25,196,248),0.4);
+    activationQueue.push(activation);
+    std::pair<Point3,float> poppedActivation = activationQueue.pop();
     return 0;
 }
